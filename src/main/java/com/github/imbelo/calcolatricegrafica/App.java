60c1fd2fcead7594818104dd0cc30527b6221855
ImBelo <gianmarco.beligni@gmail.com>
2025-07-18 18:01:52 +0200

    meow

--- src/main/java/com/github/imbelo/calcolatricegrafica/App.java
+++ src/main/java/com/github/imbelo/calcolatricegrafica/App.java
@@ -1,3 +1,6 @@
-public static void main(String[] args){
-  System.out.println("Compila!");
-} 
+package com.github.imbelo.calcolatricegrafica; 
+public class App{
+  public static void main(String[] args){
+    System.out.println("Compila!");
+  } 
+}
